bfb6632f39652a9e332157116e6c50341c704523
rakibulislam161363 <rakibulislam161363@gmail.com>
2024-11-06 08:03:38 +0600

    just

---
+++ script.js
@@ -0,0 +1,8 @@
+const Tag = document.getElementById('htmlTag');
+
+const swich = document.getElementById('change');
+
+swich.addEventListener("click", function () {
+  
+Tag.classList.toggle("dark");
+});
