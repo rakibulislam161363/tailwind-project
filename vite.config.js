2bb9cb9ff06b4ad17f9e9b5777615b704beddcb4
rakibulislam161363 <rakibulislam161363@gmail.com>
2024-11-06 20:43:06 +0600

    this is solved maby

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,8 @@ export default defineConfig({
 
   build: {
     rollupOptions: {
-      main: resolve(__dirname, "index.html"),
-      news: resolve(__dirname, "news.html"),
-      jsfile: resolve(__dirname, "script.js"),
+      main : resolve(__dirname, "index.html"),
+      news : resolve(__dirname, "news.html"),
     },
   },
 });
